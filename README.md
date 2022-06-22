d48c880cccba6b7bddb1e5b0cac1a06dd9091a17
sandanarajeev <sandanarajeev@gmail.com>
2022-06-23 01:05:46 +0530

    INIT

---
+++ README.md
@@ -0,0 +1,81 @@
+Install the dependencies
+
+WINDOWS
+
+```
+py -m pip install MetaTrader5
+py -m pip install Flask
+```
+
+Start the server
+
+```
+py mt5BridgeServer.py
+```
+
+MAC
+
+Install the dependencies
+
+```
+pip install MetaTrader5
+pip install Flask
+```
+
+Start the server
+
+```
+python mt5BridgeServer.py
+```
+
+NOTE: pip3, pythons 3 also works
+
+Start the server
+
+Windows
+
+```
+py mt5BridgeServer.py
+```
+
+MAC
+
+```
+python mt5BridgeServer.py
+```
+
+ENDPOINT = http://localhost:5001/mt5
+
+Replace localhost with Machine IP address when in network
+
+Sample Request Body
+
+To call mt5.initialize()
+
+```
+{
+    "methodName": "initialize",
+}
+```
+
+To call mt5.initialize(1,2,3)
+
+```
+{
+    "methodName": "initialize",
+    "args":[1,2,3]
+}
+```
+
+To call mt5.initialize(x=1,y=2,z=3)
+
+```
+{
+    "methodName": "initialize",
+    "args": {
+        x:1,
+        y:2,
+        z:3
+    }
+}
+```
---
+++ mt5BridgeServer.py
@@ -0,0 +1,43 @@
+from flask import Flask, request
+import MetaTrader5 as mt5
+api = Flask(__name__)
+
+
+@api.route('/mt5', methods=['POST'])
+def callMethods():
+    try:
+        if not request.is_json:
+            return {
+                'message': "Incorrect data"
+            }, 404
+        data = request.get_json()
+        print("data", data)
+        methodName = data['methodName']
+        args = None
+        if ('args' in data):
+            args = data['args']
+        objectToUse = mt5
+        print("Attempt to call mt5.", methodName, "with", args)
+        if not hasattr(objectToUse, methodName):
+            return {
+                'message': "Method does not exist"
+            }, 404
+        else:
+            method = getattr(objectToUse, methodName)
+            methodResult = None
+            if isinstance(args, list):
+                methodResult = method(*args)
+            elif isinstance(args, dict):
+                methodResult = method(**args)
+            else:
+                methodResult = method()
+        print("methodResult ", methodResult)
+        return {'data': methodResult}, 200
+
+    except Exception as e:
+        print("Error :", e)
+        return {'message': "Something went wrong"}, 500
+
+
+if __name__ == '__main__':
+    api.run(port=5001)
